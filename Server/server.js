3413dbbdaba8fe61a5be034fb00d061cead7ef2a
Hanif Carroll <HCarrollUSF@gmail.com>
2017-07-16 14:23:14 -0400

    Add /GET todos/:id

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/Todo');
@@ -13,13 +14,30 @@ app.post('/todos', (req, res) => {
 	const todo = new Todo({
 		text: req.body.text
 	});
-	todo.save().then(doc => res.send(doc)).catch(err => res.status(400).send(err));
+	todo.save().then(doc => {
+		res.send(doc);
+	}, (e) => res.status(400).send(e)
+	);
 });
 
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
-		res.send({ todos }).catch(err => res.status(400).send(err));
-	});
+		res.send({ todos });
+	}, (e) => res.status(400).send(e)
+	);
+});
+
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => res.status(400).send());
 });
 
 
