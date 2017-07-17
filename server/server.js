66a13d18ab089dc3ffb1c8903f42df08cf501eb5
Hanif Carroll <HCarrollUSF@gmail.com>
2017-07-16 21:07:38 -0400

    Add /PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/Todo');
@@ -58,8 +59,33 @@ app.delete('/todos/:id', (req, res) => {
 		// if doc, send it back with 200
 		res.send({todo});
 	}).catch(e => res.status(400).send());
-
 });
+
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	// Set what properties the user can update with PATCH
+	const body = _.pick(req.body, ['text', 'completed']);
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
