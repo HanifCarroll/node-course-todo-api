ec9f8440d58a59a6448f411dfbf4030605ce20fe
Hanif Carroll <HCarrollUSF@gmail.com>
2017-07-17 14:35:34 -0400

    Add POST /users route and token method

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,55 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		minlength: 1,
 		required: true,
-		trim: true
-	}
+		trim: true,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{value} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
 
+userSchema.methods.toJSON = function() {
+	const user = this;
+	const userObject = user.toObject();
+
+	return _.pick(userObject, ['_id', 'email']);
+};
+
+userSchema.methods.generateAuthToken = function () {
+	const user = this;
+	const access = 'auth';
+	const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+	user.tokens.push({access, token});
+
+	user.save().then(() => {
+		return token;
+	});
+};
 const User = mongoose.model('User', userSchema);
 
 module.exports = {User};
